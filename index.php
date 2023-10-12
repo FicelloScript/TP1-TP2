b4f71912084cf7f7a865a45945f7a8e291f095d6
FicelloScript <faissel.friouchen@gmail.com>
2023-10-12 16:15:25 +0200

    Interface

--- index.php
+++ index.php
@@ -20,3 +20,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Compte Bancaire</title>
+</head>
+<body>
+    <h1>Gestion de Comptes Bancaires</h1>
+    
+    <form method="post" action="index.php">
+        <label for="nom">Nom du titulaire :</label>
+        <input type="text" name="nom" required>
+        <br>
+
+        <label for="solde">Solde initial :</label>
+        <input type="number" name="solde" value="1000" required>
+        <br>
+
+        <label for="operation">Opération :</label>
+        <select name="operation">
+            <option value="depot">Dépôt</option>
+            <option value="retrait">Retrait</option>
+        </select>
+        <br>
+
+        <label for="somme">Somme :</label>
+        <input type="number" name="somme" required>
+        <br>
+
+        <input type="submit" value="Effectuer l'opération">
+    </form>
+
+    <?php
+    // Afficher le compte après l'opération
+    if (isset($compte)) {
+        $compte->affiche();
+    }
+    ?>
+
+</body>
+</html>
